8c5b0f72b920030a170658d3ad77353149eeabcb
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-03-16 14:36:03 -0300

    Toast and Count logic added

--- app/src/main/java/com/projects/hellotoast/MainActivity.java
+++ app/src/main/java/com/projects/hellotoast/MainActivity.java
@@ -3,12 +3,31 @@ package com.projects.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        count++;
+        if (textView != null) {
+            textView.setText(Integer.toString(count));
+        }
     }
 }
